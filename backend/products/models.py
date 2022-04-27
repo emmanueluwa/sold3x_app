81d5372794993b86543f5e173cbba2b1b3bb7650
emmanueluwa <emmanuel@fulodev.com>
2022-04-27 11:28:00 +0100

    adding product model

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,14 @@
+from cgi import print_exception
+import imp
+from wsgiref.validate import validator
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Product(models.Model):
+    thumbnail = models.URLField(max_length=500)
+    url = models.URLField(max_length=500)
+    title = models.CharField(max_length=250)
+    condition = models.CharField(max_length=100)
+    price = models.FloatField(validators=[MinValueValidator(0)])
+    sold_time = models.DateTimeField()
+    listing_type = models.CharField(max_length=50)
